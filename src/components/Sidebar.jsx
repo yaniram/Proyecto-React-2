117901024fd6e250e593da524cff7f9ae494ab60
Yanira Monsalve <yaniramonsalvef@gmail.com>
2021-10-15 12:56:46 -0500

    importar libreria fotawesome

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ImagenLogo from './ImagenLogo';
 import { Link } from 'react-router-dom';
 import useActiveRoute from 'hooks/useActiveRoute';
