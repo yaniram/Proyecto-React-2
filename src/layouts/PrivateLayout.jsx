96ba5a37eb1144e93e4d8b6cc9ecf2c5fa9baaae
Yanira Monsalve <yaniramonsalvef@gmail.com>
2021-10-16 21:59:37 -0500

    Autenticación Front

--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -1,14 +1,20 @@
+import PrivateRoute from 'components/PrivateRoute';
 import Sidebar from 'components/Sidebar';
 import React from 'react'
 
+
 const PrivateLayout = ({ children }) => {
+    
     return (
-     <div className="flex w-screen h-screen">
+      <PrivateRoute>
+        <div className="flex w-screen h-screen">
          <Sidebar />
          <main className="flex w-full  overflow-y-scroll items-center justify-center">
              {children}</main>
          
-     </div>
+        </div>
+
+       </PrivateRoute>
     );
     
 };
