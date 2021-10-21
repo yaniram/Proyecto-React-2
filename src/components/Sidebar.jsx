c7877f41c438d927cfda1e3b35f3f56df0066413
Yanira Monsalve <yaniramonsalvef@gmail.com>
2021-10-21 17:09:39 -0500

    Perfil Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -22,15 +22,15 @@ const Sidebar = () => {
         <Ruta icono='fas fa-user' ruta='/admin/perfil' nombre='Perfil' usuario={user} />
         <Ruta icono='fas fa-fill-drip' ruta='/admin/cartuchos' nombre='Cartuchos' />
         <Ruta icono='fas fa-cash-register' ruta='/admin/ventas' nombre='Ventas' />
-  {/*     <Ruta icono='fas fa-users' ruta='/admin/usuarios' nombre='Usuarios' /> */}
+       <Ruta icono='fas fa-users' ruta='/admin/usuarios' nombre='Usuarios' /> 
       </div>
       <button onClick={() => cerrarSesion()}>Cerrar Sesión</button>
     </nav>
   );
 };
 
-const Ruta = ({ icono, ruta, nombre }) => {
-//  console.log('usuario', usuario);
+const Ruta = ({ icono, ruta, nombre, usuario }) => {
+ console.log('usuario', usuario);
   const isActive = useActiveRoute(ruta);
   return (
     <Link to={ruta}>
@@ -39,12 +39,12 @@ const Ruta = ({ icono, ruta, nombre }) => {
        isActive ? 'indigo' : 'gray'
         }-700 hover:bg-indigo-900 flex w-full items-center text-white rounded-md`}
       >
- {/*       {usuario ? (
+        {usuario ? (
           <>
             <img src={usuario.picture} className='h-5 w-5 rounded-full' />
             {usuario.name}
           </>
- ) : (*/}
+        ) : (
           <>
             <i className={`${icono} w-10`} />
             {nombre}
